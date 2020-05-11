82c58851e05e24ba59394b8a92109a052c54d463
panchenelo <45730607+panchenelo@users.noreply.github.com>
2020-05-11 17:25:24 +0300

    Create translater.js

---
+++ translater.js
@@ -0,0 +1,178 @@
+var new_lang = {
+
+"lt0": "Sito ufficiale governativo italiao  <a aria-controls=\"gov-banner\" aria-expanded=\"false\" class=\"usa-accordion-button usa-banner-button\”>Ecco come capirtlo</a>",
+
+"lt7": "Informazioni certificate dall’Istituto Nazionale Sanità",
+
+"lt8": "Menu",
+
+"lt9": "Salute A-Z",
+
+"lt10": "Ansia",
+
+"lt11": "Pressione sanguigna",
+
+"lt12": "Marijuana",
+
+"lt13": "Endometriosi",
+
+"lt14": "HPV",
+
+"lt15": "Ipotiroidismo",
+
+"lt16": "Problemi cellulari",
+
+"lt17": "Danni alla salute",
+
+"lt18": "Vedi tutti gli argomenti",
+
+"lt19": "Ricerca INS",
+
+"lt20": "Punti salienti delle ricerche",
+
+"lt21": "Scoperte tecnologiche INS",
+
+"lt22": "Incontra i ricercatori",
+
+"lt23": "Risorse INS",
+
+"lt24": "Probeli",
+
+"lt25": "Problemi attuali",
+
+"lt26": "Problemi passati",
+
+"lt27": "Problemi risolti",
+
+"lt28": "Iscriviti",
+
+"lt29": "Versione per la stampa gratuita",
+
+"lt30": "Ricerca",
+
+"lt31": "<i class=\"fa fa-search\"></i> Ricerca",
+
+"lt32": "Raffreddore",
+
+"lt33": "Scoperta dei ricercatori: trovato immuno-modulatore universale per difendersi dai virus",
+
+"lt35": "Le infezioni respiratorie acute sono le malattie più comuni al mondo. Secondo l'Organizzazione mondiale della sanità (OMS), un adulto soffre di malattie catarrali 2-4 volte l'anno, adolescenti e bambini piccoli,circa 6 volte, neonati, da 8 a 12 volte. Negli ultimi mesi, la situazione è diventata più spaventosa a causa dell'emergere di un nuovo coronavirus mortale.",
+
+"lt36": "Gli scienziati dell’Istituto Nazionale Salute (INS) hanno studiato per molti anni l'elevata incidenza di infezioni respiratorie. Secondo i dati ottenuti durante lo studio, il <b> problema è la mancanza di connessione tra due tipi di difesa immunitaria umana: </b> cellulare e umorale.",
+
+"lt37": "Per cinque anni, gli scienziati dell’INS hanno creato un immuno-modulatore naturale universale in grado di stimolare e regolare il lavoro congiunto di entrambi i tipi di difesa immunitaria.",
+
+"lt38": "A quanto pare, l'olio di cannabis da laboratorio ha questa proprietà. Questo agente lega le immunità cellulari e umorali attraverso il sistema endocannabinoide.",
+
+"lt39": "Il sistema endocannabinoide umano risponde di molte funzioni di base, come dolore, appetito, memoria, umore, sonno e, soprattutto, delle <b> difese immunitarie </b> del corpo. La funzione principale di questo sistema è l'omeostasi: regolazione del sistema corporeo e mantenimento dell'equilibrio dell'ambiente interno in qualsiasi circostanza.",
+
+"lt40": "<b>XX</b> è il nome che è stato dato a questo preparato. Agisce sul sistema endocannabinoide attraverso ricettori particolari situati sulla fascia superiore delle cellule. Grazie a questo preparato migliora nettamente la resistenza dell’organismo alle infezioni da virus, aumenta la quantità di linfociti T e migliora la produzione di anticorpi.",
+
+"lt41": "Le ultime ricerche cliniche su<a href=\"\">XX</a> si sono concluse l’11 marzo. Uno studio condotto su un gruppo di 1000 volontari ha dimostrato che XX aiuta a far fronte a tutti i principali tipi di raffreddore. Al momento, come esperimento, i medici usano questo farmaco nella lotta contro il nuovo tipo di coronavirus.",
+
+"lt42": "Per via della grande richiesta ora <a href=\"\">XX</a> può essere acquistato solo sul <a href=\”\">sito del produttore</a>.",
+
+"lt43": "Ricordate: la vostra salute e la salute dei vostri cari dipendono solo da voi. Affrettatevi a comprare XX, finché è disponibile per l’ordine.",
+
+"lt44": "Avete ancora tempo per ordinare XX. Lo sconto smetterà di funzionare tra <span class=\"counter-minutes\">09</span>: <span class=\"counter-seconds\">49</span>",
+
+"lt45": "Scegliete la nazione:",
+
+"lt46": "Per favore, inserite il nome:",
+
+"lt47": "Inserite il numero di telefono:",
+
+"lt48": "ordinare ora con lo sconto",
+
+"lt49": "Commenti",
+
+"lt50": "Federico Seme",
+
+"lt52": "Ordinato! Grazie",
+
+"lt53": "Ludovico Ferragamo",
+
+"lt55": "Sembra la verità. Io ho grossi problemi con le difese immunitarie quando si tratta di raffreddore. Non ho mai altro, solo il raffreddore di continuo! Ho ordinato XX, mi ci curerò",
+
+"lt56": "Valeria Seno",
+
+"lt58": "Almeno una buona notizia tra tutte queste notizie tremende sul coronavirus!",
+
+"lt59": "Elisabetta Martini",
+
+"lt61": "Ne ho sentito parlare in TV! Ho compilato il modulo sul sito",
+
+"lt62": "Giorgio Castori",
+
+"lt64": "Anche io l’ho ordinato. La prevenzione ora è molto importante per quanto riguarda i virus",
+
+"lt65": "Federica Villalba",
+
+"lt67": "Articolo molto bello! Un’informazione molto importante",
+
+"lt68": "Anna Pipolo",
+
+"lt70": "Sembrerebbe interessante",
+
+"lt71": "Carla Forensi",
+
+"lt73": "Quello che mi serviva. Ordinato!",
+
+"lt74": "Marina Puledro",
+
+"lt77": "Rocco Piloni",
+
+"lt79": "Ci troviamo in un periodo in cui senza prodotti che funzionano è la fine.",
+
+"lt80": "Disturbi stagionali",
+
+"lt81": "Ricadute: disturbi stagionali",
+
+"lt82": "Questo novembre la maggior parte di noi riporterà indietro gli orologi quando terminerà l'ora legale, segno che l'inverno sta arrivando di nuovo ...",
+
+"lt83": "influenza",
+
+"lt84": "ETCIU! raffreddore, influenza o altro?",
+
+"lt85": "L'inverno e l'inizio della primavera in genere portano raffreddori, influenza e allergie, anche se possono verificarsi in qualsiasi momento dell'anno ....",
+
+"lt86": "8600 Rockville Pike<br>Bethesda, MD 20894",
+
+"lt87": "MedlinePlus",
+
+"lt88": "Informazioni sul negozio",
+
+"lt89": "Servizio clienti",
+
+"lt90": "Su di noi",
+
+"lt91": "Disclaimers",
+
+"lt92": "Copyright",
+
+"lt93": "Accessibilità",
+
+"lt94": "Privacy",
+
+"lt95": "NLM",
+
+"lt96": "INS",
+
+"lt97": "HHS",
+
+"lt98": "USA.gov",
+
+"lt103": "Vai all’inizio",
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
